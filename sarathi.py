60a5137f3e60650fc686cc3adc22adec9b6d410c
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-18 12:07:21 +0530

    condition statement Task

--- sarathi.py
+++ sarathi.py
@@ -1,31 +1,58 @@
-####Exercise:3
-####Bitwise operator
-##p=5
-##q=8
-##print(p^q)
-##print(~p)
-####Exercise:2
-####boolean and logical operator
-##is_sunny=False
-##is_warm=True
-##is_sunny_and_warm=is_sunny and is_warm or is_sunny
-##print("it is sunny and warm:",is_sunny_and_warm)
-##Exercise:1
-##num=int(input("enter your number:"))
-##num +=3
-##num *=2
-##print("the result is num",num)
-##combining string operator
-##Name=str(input("enter the name"))
-##Age=str(input("enter the age"))
-##print("My name is",Name,"and i am ",Age,"years old.")
-##problem:8
-####Modulo and even-odd check
-##even=int(input("enter the number of even"))
-##odd=int(input("enter the number of odd"))
-##number=even%odd
-##print("indicating whether number is",number)
-##
+##TASK 1:
+##age=int(input("Enter your age"))
+##if(age>=60):
+##    print("you are senior citizen")
+##else:
+##    print("you are not a senior citizen")
+####TASK 2:
+##num=int(input("Enter the number"))
+##if(num%3==0):
+##    print("the given number is odd")
+##else:
+##    print("the given number is even")
+##TASK 3;
+##positive and negative 
+##num=int(input("enter the number"))
+##if(num>=5):
+##    print("it is positive number")
+##else:
+##    print("it is negative number")
+##TASK: 4
+##salary=int(input("your company salary"))
+##mem=int(input("how many members in your company"))
+##a=salary/5
+##if(mem>=5):
+##    print("5% bonus for you")
+##else:
+##    print("5% bonus not for you")
+##task:5
+##L=int(input("enter the value of length:"))
+##B=int(input("enter the value of breath:"))
+##if(L==B):
+##    print("it is square")
+##else:
+##    print("it is not square")
+##TASK 6:
+##num=int(input("Enter the number"))
+##if(num%5==0):
+##    print("hello")
+##else:
+##    print("bye")
+##TASK:7
+##a=int(input("Number of classes held"))
+##b=int(input("Number of classes attended"))
+##percentage=(a/b)*100
+##if(percentage>=75):
+##    print("student allowed to sit in exam")
+##else:
+##    print("student not allowed to sit in exam")
+
+
+
+
+
+
+
 
 
 
